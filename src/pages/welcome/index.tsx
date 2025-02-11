de11b4531060ee63a00b2699cff63177063a7d42
AlbertoNietoDev <albertonietocovarrubias@gmail.com>
2025-02-11 10:40:43 -0600

    feature: new section

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -31,21 +31,12 @@ export const Welcome: React.FC = () => {
 
   //Render
   return (
-    // <GenericContainer primary={palette?.primary}>
-      <Box
-            sx={{
-              backgroundImage:`url(https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w7680-h4320-rw)`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-             /*  boxShadow:
-                "inset 18.0333px -18.0333px 18.0333px rgba(194, 194, 194, 0.1), inset -18.0333px 18.0333px 18.0333px rgba(255, 255, 255, 0.1)",
-              backdropFilter: "blur(18.0333px)", */
-            }}
-          >
-      {/* <HeaderDrawer mainStyle={welcomeStyles} OnRedirect={handleRedirect} />
-      <BodyCustom mainStyle={welcomeStyles} /> */}
-      </Box>
-    // </GenericContainer>
+    <GenericContainer primary={palette?.primary}>
+      <GeneralBackground />
+      <HeaderDrawer mainStyle={welcomeStyles} OnRedirect={handleRedirect} />
+      <BodyCustom mainStyle={welcomeStyles} />
+      <Box sx={{zIndex: 100, height: '90vh', width: "100%", border: '2px solid red'}}>adads</Box>
+    </GenericContainer>
   );
 };
 
@@ -53,8 +44,7 @@ const HeaderDrawer: React.FC<HeaderProps> = ({ mainStyle, OnRedirect }) => {
   return (
     <Box className={mainStyle.headerDrawer}>
       <Typography variant="h3">
-        {" "}
-        <span style={{ color: "rgb(21, 40, 176)" }}>{`{:}`}</span> Developer X
+        <span className={mainStyle.spanTitleSyle}>{`{ : }`}</span> Developer X
       </Typography>
       <Stack
         sx={{
@@ -76,9 +66,9 @@ const HeaderDrawer: React.FC<HeaderProps> = ({ mainStyle, OnRedirect }) => {
   );
 };
 
-const BodyCustom: React.FC<BodyProps> = () => {
+const BodyCustom: React.FC<BodyProps> = ({ mainStyle }) => {
   return (
-    <Stack sx={{ width: "100%", px: 2, height: "90vh" }}>
+    <Stack className={mainStyle.bodyCustom}>
       <Box
         display={"flex"}
         flexDirection={"row"}
@@ -104,11 +94,13 @@ const BodyCustom: React.FC<BodyProps> = () => {
           <Stack gap={2} sx={{ width: "50%", py: 5 }}>
             <Typography variant="subtitle2">ABOUT ME</Typography>
             <Typography variant="subtitle1">
-              Soy un desarrollador web con ingenieria en teleinformatica y con gusto por el desarrollo web y siempre dispuesto a aprender mas y poner retos personales y profesionales
+              Soy un desarrollador web con ingenieria en teleinformatica y con
+              gusto por el desarrollo web y siempre dispuesto a aprender mas y
+              poner retos personales y profesionales
             </Typography>
             <Box
               sx={{
-                width: '8.5em',
+                width: "8.5em",
                 display: "flex",
                 flexDirection: "row",
                 gap: 1,
@@ -124,16 +116,20 @@ const BodyCustom: React.FC<BodyProps> = () => {
             </Box>
           </Stack>
 
-          <Divider sx={{ backgroundColor: '#40424f', height: '2px', width: '50%' }} />
+          <Divider
+            sx={{ backgroundColor: "#40424f", height: "2px", width: "50%" }}
+          />
 
           <Stack gap={2} sx={{ width: "50%", py: 5 }}>
             <Typography variant="subtitle2">MY WORK</Typography>
             <Typography variant="subtitle1">
-              Eh trabajado para empresas de desarrollo de software a nivel nacional e internacional trabajando para distintas empresas de renombre mundial como HSBC, Metlife, Santander, etc
+              Eh trabajado para empresas de desarrollo de software a nivel
+              nacional e internacional trabajando para distintas empresas de
+              renombre mundial como HSBC, Metlife, Santander, etc
             </Typography>
             <Box
               sx={{
-                width: '11em',
+                width: "11em",
                 display: "flex",
                 flexDirection: "row",
                 gap: 1,
@@ -147,9 +143,38 @@ const BodyCustom: React.FC<BodyProps> = () => {
             </Box>
           </Stack>
 
-          <Divider sx={{ backgroundColor: '#40424f', height: '2px', width: '50%' }} />
+          <Divider
+            sx={{ backgroundColor: "#40424f", height: "2px", width: "50%" }}
+          />
         </Stack>
       </Box>
     </Stack>
   );
 };
+
+const GeneralBackground: React.FC = () => {
+  const generalStyle = { width: "100%", height: "100vh", position: "fixed" };
+  return (
+    <>
+      <Box
+        sx={{
+          ...generalStyle,
+          zIndex: 1,
+          backgroundImage:
+            "url(https://eldalab.in/wp-content/uploads/2023/08/web-development.jpg)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <Box
+        sx={{
+          ...generalStyle,
+          zIndex: 2,
+          backdropFilter: "blur(18.0333px)",
+        }}
+      />
+    </>
+  );
+};
+
--- src/pages/welcome/welcomeStyle.tsx
+++ src/pages/welcome/welcomeStyle.tsx
@@ -2,6 +2,7 @@ import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles((theme: any) => ({
   headerDrawer: {
+    zIndex: 100,
     width: "100%",
     flexShrink: 0,
     display: "flex",
@@ -10,5 +11,17 @@ export const useStyles = makeStyles((theme: any) => ({
     alignItems: "center",
     padding: "1rem 2rem 0rem 2rem",
     height: "10vh",
+    position: "sticky", top:0
   },
+  bodyCustom: {
+    zIndex: 100,
+    width: "100%",
+    paddingLeft: '2rem',
+    paddingRight: '2rem',
+    minHeight: "90vh",
+  },
+  spanTitleSyle: {
+    color: "rgb(50, 73, 245)",
+    // textShadow: "0px 0px 10px rgb(255, 255, 255)",
+  }
 }));
